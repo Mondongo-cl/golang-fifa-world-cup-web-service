1915c202846f32a0e39db432c3b69150509930bb
Ivan Rosales Rieloff <ivan.rosales.rieloff@gmail.com>
2021-07-13 20:27:48 -0400

    Completed module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -39,6 +39,20 @@ func writeServerError(res http.ResponseWriter, err error) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+	if !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+		return
+	}
 
 }
 
